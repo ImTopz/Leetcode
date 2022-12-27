a85166256514fff8172d9c921fdc90446321fdc6
ImTopz <1976521709@qq.com>
2022-12-27 22:00:04 +0800

    二分查找例题

--- FirstBadVersion.java
+++ FirstBadVersion.java
@@ -1,7 +1,7 @@
 /* The isBadVersion API is defined in the parent class VersionControl.
       boolean isBadVersion(int version); */
 
-public class FirstBadVersion extends VersionControl {
+public class Solution extends VersionControl {
     public int firstBadVersion(int n) {
         int min = n;
         int l = 0;
